1cb4f9af5a093303e4c9c0126034e5ae6db28369
Anderson Ladislau dos Santos <73796983+andersonlsantos@users.noreply.github.com>
2022-07-11 16:49:05 -0300

    Dice

--- script.js
+++ script.js
@@ -5,7 +5,27 @@ const sell = e => document.querySelector(`${e}`)
 const sellAll = e => document.querySelectorAll(`${e}`)
 
 const  rand = () => {
-    let rnadi = Math.random() * 6
+    let randi = Math.trunc(Math.random() * 6)
+    switch(randi){
+        case 0:
+            return 'dice-1.png'
+            break
+        case 1:
+            return 'dice-2.png'
+            break
+        case 2:
+            return 'dice-3.png'
+            break
+        case 3:
+            return 'dice-4.png'
+            break
+        case 4:
+            return 'dice-4.png'
+            break
+        case 5:
+            return 'dice-6.png'
+            break
+    }
 }
 
-console.log(rand)
+sell('.btn--roll').addEventListener('click', () => sell('.dice').src = `${rand()}`)
