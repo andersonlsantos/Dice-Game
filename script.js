b1139b2e159d51536a826238c49066284250c537
Anderson Ladislau dos Santos <73796983+andersonlsantos@users.noreply.github.com>
2022-07-11 15:55:35 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const sell = e => document.querySelector(`${e}`)
+
+const sellAll = e => document.querySelectorAll(`${e}`)
+
+const  rand = () => {
+    let rnadi = Math.random() * 6
+}
+
+console.log(rand)
