8822d516281cb893262ae909857082a464e41bcf
Anderson Ladislau dos Santos <73796983+andersonlsantos@users.noreply.github.com>
2022-07-15 16:35:07 -0300

    arr

--- script.js
+++ script.js
@@ -6,26 +6,31 @@ const sellAll = e => document.querySelectorAll(`${e}`)
 
 const  rand = () => {
     let randi = Math.trunc(Math.random() * 6)
+    let arr = []
     switch(randi){
         case 0:
-            return ['dice-1.png', 1]
+            arr.push('dice-1.png', 1)
             break
         case 1:
-            return ['dice-2.png', 2]
+            arr.push('dice-2.png', 2)
             break
         case 2:
-            return ['dice-3.png', 3]
+            arr.push('dice-3.png', 3)
             break
         case 3:
-            return ['dice-4.png', 4]
+            arr.push('dice-4.png', 4)
             break
         case 4:
-            return ['dice-5.png', 5]
+            arr.push('dice-5.png', 5)
             break
         case 5:
-            return ['dice-6.png', 6]
+            arr.push('dice-6.png', 6)
             break
     }
+    return arr
 }
 
-sell('.btn--roll').addEventListener('click', () => sell('.dice').src = `${rand()[0]}`)
+sell('.btn--roll').addEventListener('click', () => {
+    sell('.dice').src = `${rand()[0]}`
+    console.log(rand())
+})
