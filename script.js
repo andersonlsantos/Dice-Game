095b32923544dbb499f40b9e421f9f7582e44977
Anderson Ladislau dos Santos <73796983+andersonlsantos@users.noreply.github.com>
2022-07-15 15:48:35 -0300

    rand

--- script.js
+++ script.js
@@ -8,24 +8,24 @@ const  rand = () => {
     let randi = Math.trunc(Math.random() * 6)
     switch(randi){
         case 0:
-            return 'dice-1.png'
+            return ['dice-1.png', 1]
             break
         case 1:
-            return 'dice-2.png'
+            return ['dice-2.png', 2]
             break
         case 2:
-            return 'dice-3.png'
+            return ['dice-3.png', 3]
             break
         case 3:
-            return 'dice-4.png'
+            return ['dice-4.png', 4]
             break
         case 4:
-            return 'dice-4.png'
+            return ['dice-5.png', 5]
             break
         case 5:
-            return 'dice-6.png'
+            return ['dice-6.png', 6]
             break
     }
 }
 
-sell('.btn--roll').addEventListener('click', () => sell('.dice').src = `${rand()}`)
+sell('.btn--roll').addEventListener('click', () => sell('.dice').src = `${rand()[0]}`)
